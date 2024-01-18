bcafc1f7977890337834c77ead51d382057a114f
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-18 20:41:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
-  // your code here
+  // Split the input string by "000" to get an array of firstName, lastName, and id
+  const [firstName, lastName, id] = str.split("000");
+
+  // Return an object with properties firstName, lastName, and id
+  return {
+    firstName,
+    lastName,
+    id
+  };
 };
 
 // Do not change the code below
